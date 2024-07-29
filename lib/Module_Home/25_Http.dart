c441c9ef9868ba86587a30805396b86058958c6b
Will.H <huangpu0205@163.com>
2024-07-29 15:48:42 +0800

    get\post 请求

--- lib/Module_Home/25_Http.dart
+++ lib/Module_Home/25_Http.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:typed_data';
 
 class MyHttp extends StatefulWidget {
   const MyHttp({super.key});
@@ -11,45 +11,166 @@ class MyHttp extends StatefulWidget {
 }
 
 class _MyHttpState extends State<MyHttp> {
+   //展示请求结果
+  var resultShow = "";
+
+  //请求结果解析
+  var resultDecodeShow = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // 1、原生路由接收数据
-        //title: Text(widget.arguments["title"]),
-        // 2、Get方式路由跳转接收数据
-        title: Text(Get.arguments['title']),
+        title: const Text("Http 的 post 请求"),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          FutureBuilder<String>(
-            future: fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.requireData);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return const CircularProgressIndicator();
-            },),
-          const ListTile(
-            title: Text('get'),
+          Center(
+            child: _httpGetButton(),
           ),
-          FutureBuilder<String>(
-            future: fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                Map<String, dynamic> userMap = jsonDecode(snapshot.requireData);
-                return Text(userMap["userId"].toString());
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return const CircularProgressIndicator();
-            },),
+          //post 请求按钮 - form
+          Center(
+            child: _httpPostFormButton(),
+          ),
+          //post 请求按钮 - json
+          Center(
+            child: _httpPostJsonButton(),
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: Text(
+                '请求结果：\n $resultShow',
+                textAlign: TextAlign.left,
+              )),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: Text(
+                '请求结果（解析）：\n $resultDecodeShow',
+                textAlign: TextAlign.left,
+              )),
+            ],
+          ),
         ],
       ),
     );
   }
+
+  //get 按钮
+  _httpGetButton() {
+    return ElevatedButton(onPressed: _doGet, child: const Text('发起 Get 请求'));
+  }
+
+  //post 按钮 - form
+  _httpPostFormButton() {
+    return ElevatedButton(
+        onPressed: _doFormPost, child: const Text('发起 post 请求 - form'));
+  }
+
+  //post 按钮 - json
+  _httpPostJsonButton() {
+    return ElevatedButton(
+        onPressed: _doJsonPost, child: const Text('发起 post 请求 - json'));
+  }
+
+  // utf8 解码
+  _decodeUtf8(String responseBody) {
+    var bytes = Uint8List.fromList(responseBody.codeUnits);
+    String decodeString = utf8.decode(bytes);
+    var decodeMap = json.decode(decodeString);
+    return decodeMap;
+  }
+
+  //解析结果 json 数据
+  String _decodeResult(String jsonString) {
+    var map = jsonDecode(jsonString);
+    var decodeString = "";
+    map.forEach((key, value) {
+      decodeString += "key: $key ------ value: $value\n";
+    });
+    return decodeString;
+  }
+
+  //请求成功
+  _success(http.Response response, bool isNeedUtf8) {
+    setState(() {
+      if (isNeedUtf8) {
+        //返回结果的汉字用了 utf8 编码，所以需要对结果进行 utf8 解码
+        resultShow = '请求成功：${_decodeUtf8(response.body)}';
+      } else {
+        //返回结果的汉字未用 utf8 编码，所以无需解码
+        resultShow = '请求成功：${response.body}';
+      }
+      resultDecodeShow = _decodeResult(response.body);
+    });
+  }
+
+  //请求失败
+  _failed(http.Response response) {
+    setState(() {
+      resultShow =
+          "请求失败：errorCode: ${response.statusCode}   errorMessage: ${response.body}";
+    });
+  }
+
+  //发送 Get 请求
+  _doGet() async {
+    var url =
+        Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      //请求成功
+      _success(response, true);
+    } else {
+      //请求失败
+      _failed(response);
+    }
+  }
+
+  //发送 Post 请求 - form
+  _doFormPost() async {
+    var url = Uri.parse('http://api.crap.cn/visitor/example/post.do');
+    var params = {
+      'param': '测试22',
+      'email': 'crap.cn@gmail.com',
+      '555': '我'
+    }; //如果传 Map 类型，必须是 Map<String, String>，否则会解析报错
+    var response =
+        await http.post(url, body: params); //默认是 form 类型，所以无需设置 content - type
+    if (response.statusCode == 200) {
+      //请求成功
+      _success(response, false);
+    } else {
+      //请求失败
+      _failed(response);
+    }
+  }
+
+  //发送 Post 请求 - json
+  _doJsonPost() async {
+    var url = Uri.parse('http://api.crap.cn/visitor/example/json.do');
+    var params = {
+      "id": "8989-dddvdg",
+      "name": "文章标题-JSON格式参数演示",
+      "brief": "快速入门json参数",
+      "category": "分类"
+    };
+    var json = jsonEncode(params);
+    var response = await http.post(url, body: json, headers: {
+      'content-type': 'application/json'
+    }); //设置content-type为application/json
+    if (response.statusCode == 200) {
+      //请求成功
+      _success(response, false);
+    } else {
+      //请求失败
+      _failed(response);
+    }
+  }
+  
 }
 
 
@@ -61,3 +182,5 @@ Future<String> fetchData() async {
       throw Exception('Failed to load post');
     }
   }
+
+ 
